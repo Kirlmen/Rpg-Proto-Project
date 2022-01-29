48f5ad1f3a79f835d87494551949ed897aacd2e4
Kirlmen <kirlmen0764@gmail.com>
2022-01-29 04:57:13 +0300

    Initial commit

---
+++ Assets/_Scripts/Player/Mover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    NavMeshAgent navMesh;
+
+
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveClick();
+        }
+        MovementAnimation();
+    }
+
+    private void MoveClick()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            navMesh.destination = hit.point;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * 40, Color.red);
+    }
+
+    private Vector3 playerSpeed;
+    private void MovementAnimation()
+    {
+        playerSpeed = navMesh.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(playerSpeed);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
